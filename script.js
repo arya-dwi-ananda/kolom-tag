b4caeb72165598d96135d6b0ddd57b0779f2f9c0
arya-dwi-ananda <aryadwiananda69@gmail.com>
2024-08-04 21:36:47 +0700

    full feature

---
+++ script.js
@@ -0,0 +1,19 @@
+const ul= document.querySelector("ul"),
+input= document.querySelector("input"),
+tagNumb= document.querySelector("details span");
+
+let maxTags=10, tags=["Sarjana IT", "Pemrograman Web"];
+
+countTag();
+createTag();
+
+function countTag(){
+    input.focus();
+    tagNumb.innerText= maxTags- tags.length;
+}
+function createTag(){
+    ul.querySelectorAll("li").forEach(li=>li.remove());
+    tags.slice().reverse().forEach(tags=>{
+        let liTag=`<li>${tag}<i class="uit uit-multiply" onclick="remove(this. '${tag}')"></i></li>`
+    })
+}
